a677ba1115935daae8e449d85d16442041b32f41
DmitriySi <demonishishe@gmail.com>
2022-06-18 17:47:06 +0300

    done

--- src/main/java/com/example/bootstrap/models/User.java
+++ src/main/java/com/example/bootstrap/models/User.java
@@ -32,7 +32,7 @@ public class User implements UserDetails {
     private String password;
 
 
-    @ManyToMany(fetch = FetchType.LAZY)
+    @OneToMany
     @JoinTable(name = "users_roles",joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id"),
     inverseJoinColumns = @JoinColumn(name = "role_id",referencedColumnName = "id"))
     private Set<Role> roles;
